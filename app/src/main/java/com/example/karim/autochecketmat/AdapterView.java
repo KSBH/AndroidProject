07daa588406dcfa894bca54f510cbe2568b3a69d
KSBH <soubhi.karim@gmail.com>
2017-10-06 10:07:12 +0200

    AdapterView for ListView custom done , need to populate and test that shit

--- app/src/main/java/com/example/karim/autochecketmat/AdapterView.java
+++ app/src/main/java/com/example/karim/autochecketmat/AdapterView.java
@@ -4,7 +4,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -25,4 +30,35 @@ public class AdapterView extends ArrayAdapter<String> {
         this.item_list=item_list;
 
     }
+    // Hold views of the ListView to improve its scrolling performance
+    static class ViewHolder {
+        public TextView textview;
+        public Button button1;
+        public Button button2;
+
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View rowView = convertView;
+        // Inflate the list_item.xml file if convertView is null
+        if(rowView==null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            rowView= inflater.inflate(groupid, parent, false);
+            ViewHolder viewHolder = new ViewHolder();
+            viewHolder.textview= (TextView) rowView.findViewById(R.id.txt_item);
+            viewHolder.button1= (Button) rowView.findViewById(R.id.bt_upd);
+            viewHolder.button2= (Button) rowView.findViewById(R.id.bt_del);
+            rowView.setTag(viewHolder);
+
+        }
+        // Set text to each TextView of ListView item
+        ViewHolder holder = (ViewHolder) rowView.getTag();
+        holder.textview.setText(item_list[position]);
+        holder.button1.setText(item_list[position]);
+        holder.button2.setText(item_list[position]);
+        return rowView;
+    }
+
 }
+
